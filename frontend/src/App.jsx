114900c4cb0eb4d7a4573ec3f247d25c2230a9f2
krishna <krishkrishnatrey@gmail.com>
2026-04-04 19:10:18 +0530

    Added Login page and updated route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
