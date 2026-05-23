9a6b45479300abd2cb77d4f54ae561cfa48b21d9
krishna <krishkrishnatrey@gmail.com>
2026-05-23 17:02:00 +0530

    fix cors properly for dev + prod

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,37 @@
-const exprees = require('express');
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
-const cors = require('cors');
 const interviewRoutes = require('./routes/interview.routes');
+const cors = require('cors');
 
+const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://aira-ai-xi.vercel.app"
+];
 
-const app = exprees();
-
-app.use(exprees.json());
-app.use(cookieParser());
 app.use(cors({
-    origin: 'https://aira-ai-xi.vercel.app',
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(null, false);
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.options("*", cors());
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/interview', interviewRoutes);
-
-
 
 module.exports = app;
